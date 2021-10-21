1a1f48d0fb104200afa4a6c6b86eff22f5a65ded
Shelby VandenHoek <wobee13@gmail.com>
2021-10-20 20:07:17 -0500

    Added Splashscreen and Icons

--- secure-password-manager/src/App.tsx
+++ secure-password-manager/src/App.tsx
@@ -1,45 +1,22 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Home from "./pages/Home";
+import {ChakraProvider} from "@chakra-ui/react";
+import {Route} from "wouter";
+import {invoke} from '@tauri-apps/api/tauri'
+
+document.addEventListener('DOMContentLoaded', () => {
+    // This will wait for the window to load
+    setTimeout(() => {
+        invoke('close_splashscreen')
+    }, 5000)
+})
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    return (
+        <ChakraProvider>
+            <Route path="/" component={Home}/>
+        </ChakraProvider>
+    )
 }
 
 export default App
---
+++ secure-password-manager/src/pages/Home.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import logo from '../images/Lock.svg';
+import '../styles/App.css';
+import { Button } from "@chakra-ui/react";
+
+function Home() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Hello Vite + React!</p>
+                <p>
+                    <Button bg="teal" onClick={() => setCount((count) => count + 1)}>
+                        count is: {count}
+                    </Button>
+                </p>
+                <p>
+                    Edit <code>App.tsx</code> and save to test HMR updates.
+                </p>
+                <p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    {' | '}
+                    <a
+                        className="App-link"
+                        href="https://vitejs.dev/guide/features.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Vite Docs
+                    </a>
+                </p>
+            </header>
+        </div>
+    )
+}
+
+export default Home
