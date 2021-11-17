c8fb88c9b1b9f8f5258f56cf51b7b050a032c460
Shelby VandenHoek <wobee13@gmail.com>
2021-11-17 14:52:45 -0600

    Disable Password Check

--- secure-password-manager/src/pages/Passwords.tsx
+++ secure-password-manager/src/pages/Passwords.tsx
@@ -1,25 +1,25 @@
 import {Box, Flex, FormControl, FormLabel, Input, Text} from "@chakra-ui/react";
-import IsPwned from "../api/HaveIBeenPwned";
-
-const pw = new IsPwned();
-console.log(pw.hashPassword('1234'));
-
-try {
-    await pw.check('password');
-} catch ({count, name}) {
-    switch (name) {
-        case 'UnexpectedHttpResponseError':
-        // A response other than 200 was received from HIBP
-        case 'TimedOutError':
-        // The timeout was reached
-        case 'InvalidPasswordError':
-        // The password is either not a string or is empty
-        case 'BreachedError':
-            // The password has been breached
-            // You can use e.count on this error type
-            console.log(`Password has been breached ${count} times.`);
-    }
-}
+// import IsPwned from "../api/HaveIBeenPwned";
+//
+// const pw = new IsPwned();
+// // console.log(pw.hashPassword('1234'));
+//
+// try {
+//     await pw.check('PASSWORD');
+// } catch ({count, name}) {
+//     switch (name) {
+//         case 'UnexpectedHttpResponseError':
+//         // A response other than 200 was received
+//         case 'TimedOutError':
+//         // The timeout was reached
+//         case 'InvalidPasswordError':
+//         // The password is either not a string or is empty
+//         case 'BreachedError':
+//             // The password has been breached
+//             // You can use e.count on this error type
+//             console.log(`Password has been breached ${count} times.`);
+//     }
+// }
 export default function Passwords() {
 
     return (
